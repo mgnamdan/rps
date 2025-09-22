2ec89340936509f81840ed0e9d918a635686aee7
mgnamdan <danny.ai.hub@gmail.com>
2025-09-22 10:19:03 -0400

    finished basic game

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+from random import randint
+
 # 1. Turn on the application -> X
 # 2. Ask if user wants to play -> X
 # 3. If they don't want to play, shut off application -> X
-# 4. If they do want to play, asking what move they want
-# 5. Decide computer's move
-# 6. Handle winner
-# 7. Repeat steps 2-7
+
+# 4. If they do want to play, asking what move they want -> X
+# 5. Decide computer's move -> X
+# 6. Handle winner -> X
+# 7. Repeat steps 2-7 -> X
+
+def decideWinner(pChoice, cChoice):
+    if (pChoice == "rock"):
+        if (cChoice == "rock"):
+            print("You tied!")
+        elif (cChoice == "paper"):
+            print("You lose!")
+        else:
+            print("You win!")
+
+    elif (pChoice == "paper"):
+        if (cChoice == "rock"):
+            print("You win!")
+        elif (cChoice == "paper"):
+            print("You tie!")
+        else:
+            print("You lose!")
+
+    elif (pChoice == "scissors"):
+        if (cChoice == "rock"):
+            print("You lose!")
+        elif (cChoice == "paper"):
+            print("You win!")
+        else:
+            print("You tie!")
+
+    else:
+        print("Invalid choice! Try again!")
+
 
 def main():
     gameOn = True
+
+    choices = {1: "rock",
+               2: "paper",
+               3: "scissors"}
 
     while gameOn:
         play = input("Would you like to play rock, paper, scissors? (y/n): ").lower()
@@ -15,7 +51,14 @@ def main():
         if (play == "n" or play == "no"):
             gameOn = False
         else: 
-            pass
+            playerChoice = input("Choose a move (rock/paper/scissors): ").lower()
+            computerChoice = choices[randint(1, 3)]
+
+            print(f"Your choice was: {playerChoice}")
+            print(f"The computer's choice was: {computerChoice}")
+
+            decideWinner(playerChoice, computerChoice)
+
 
 
 
